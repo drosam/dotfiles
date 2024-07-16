f68a4c0e2ab571688280368a9f0ecd9348dc0066
David Rosa Moncayo <david.rosamoncayo@gmail.com>
2024-07-16 22:20:59 +0100

    Move to new which key format

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,28 +10,15 @@ vim.keymap.set(
 
 -- Run tests
 local wk = require("which-key")
-wk.register({
-  t = {
-    name = "Tests",
-    t = { "<cmd>lua require'david_tests'.run_file()<CR>", "Run tests for file" },
-    l = { "<cmd>lua require'david_tests'.run_line()<CR>", "Run tests for line" },
-    y = { "<cmd>lua require'david_tests'.run_last()<CR>", "Run last test" },
-  },
-}, { prefix = "<leader>" })
+wk.add({
+  { "<leader>t", group = "Tests" },
+  { "<leader>tl", "<cmd>lua require'david_tests'.run_line()<CR>", desc = "Run tests for line" },
+  { "<leader>tt", "<cmd>lua require'david_tests'.run_file()<CR>", desc = "Run tests for file" },
+  { "<leader>ty", "<cmd>lua require'david_tests'.run_last()<CR>", desc = "Run last test" },
+})
 
 -- Close other buffers
-vim.keymap.set(
-  "n",
-  "<leader>bo",
-  ":%bd|e#|bd#<CR>|'",
-  { desc = "Delete other buffers", silent = true }
-)
+vim.keymap.set("n", "<leader>bo", ":%bd|e#|bd#<CR>|'", { desc = "Delete other buffers", silent = true })
 
 -- Close all buffers
-vim.keymap.set(
-  "n",
-  "<leader>ba",
-  ":%bd<CR>|'",
-  { desc = "Delete all buffers", silent = true }
-)
-
+vim.keymap.set("n", "<leader>ba", ":%bd<CR>|'", { desc = "Delete all buffers", silent = true })
