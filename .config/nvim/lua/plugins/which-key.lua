1b7d5db25040fd991c16808d3e943a25f0165673
David Rosa Moncayo <david.rosamoncayo@gmail.com>
2025-01-22 23:18:12 +0000

    Improve mini-ai

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,72 +1,128 @@
 return {
-{
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts_extend = { "spec" },
-    opts = {
-      preset = "classic",
-      defaults = {},
-      spec = {
-        {
-          mode = { "n", "v" },
-          { "<leader><tab>", group = "tabs" },
-          { "<leader>c", group = "code" },
-          { "<leader>d", group = "debug" },
-          { "<leader>dp", group = "profiler" },
-          { "<leader>f", group = "file/find" },
-          { "<leader>g", group = "git" },
-          { "<leader>gh", group = "hunks" },
-          { "<leader>q", group = "quit/session" },
-          { "<leader>s", group = "search" },
-          { "<leader>t", group = "tests" },
-          { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
-          { "<leader>uc", group = "copilot" },
-          { "<leader>ut", group = "treesitter-context" },
-          { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
-          { "[", group = "prev" },
-          { "]", group = "next" },
-          { "g", group = "goto" },
-          { "gs", group = "surround" },
-          { "z", group = "fold" },
-          {
-            "<leader>b",
-            group = "buffer",
-            expand = function()
-              return require("which-key.extras").expand.buf()
-            end,
-          },
-          {
-            "<leader>w",
-            group = "windows",
-            proxy = "<c-w>",
-            expand = function()
-              return require("which-key.extras").expand.win()
-            end,
-          },
-          -- better descriptions
-          { "gx", desc = "Open with system app" },
-        },
-      },
-    },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Keymaps (which-key)",
-      },
-      {
-        "<c-w><space>",
-        function()
-          require("which-key").show({ keys = "<c-w>", loop = true })
-        end,
-        desc = "Window Hydra Mode (which-key)",
-      },
-    },
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-    end,
-  }
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts_extend = { "spec" },
+		opts = {
+			preset = "classic",
+			defaults = {},
+			spec = {
+				{
+					mode = { "n", "v" },
+					{ "<leader><tab>", group = "tabs" },
+					{ "<leader>c", group = "code" },
+					{ "<leader>d", group = "debug" },
+					{ "<leader>dp", group = "profiler" },
+					{ "<leader>f", group = "file/find" },
+					{ "<leader>g", group = "git" },
+					{ "<leader>gh", group = "hunks" },
+					{ "<leader>q", group = "quit/session" },
+					{ "<leader>s", group = "search" },
+					{ "<leader>t", group = "tests" },
+					{ "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+					{ "<leader>uc", group = "copilot" },
+					{ "<leader>ut", group = "treesitter-context" },
+					{ "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+					{ "[", group = "prev" },
+					{ "]", group = "next" },
+					{ "g", group = "goto" },
+					{ "gs", group = "surround" },
+					{ "z", group = "fold" },
+					{
+						"<leader>b",
+						group = "buffer",
+						expand = function()
+							return require("which-key.extras").expand.buf()
+						end,
+					},
+					{
+						"<leader>w",
+						group = "windows",
+						proxy = "<c-w>",
+						expand = function()
+							return require("which-key.extras").expand.win()
+						end,
+					},
+					-- better descriptions
+					{ "gx", desc = "Open with system app" },
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Keymaps (which-key)",
+			},
+			{
+				"<c-w><space>",
+				function()
+					require("which-key").show({ keys = "<c-w>", loop = true })
+				end,
+				desc = "Window Hydra Mode (which-key)",
+			},
+		},
+		config = function(_, opts)
+			local wk = require("which-key")
+			wk.setup(opts)
+
+			local objects = {
+				{ " ", desc = "whitespace" },
+				{ '"', desc = '" string' },
+				{ "'", desc = "' string" },
+				{ "(", desc = "() block" },
+				{ ")", desc = "() block with ws" },
+				{ "<", desc = "<> block" },
+				{ ">", desc = "<> block with ws" },
+				{ "?", desc = "user prompt" },
+				{ "U", desc = "use/call without dot" },
+				{ "[", desc = "[] block" },
+				{ "]", desc = "[] block with ws" },
+				{ "_", desc = "underscore" },
+				{ "`", desc = "` string" },
+				{ "a", desc = "argument" },
+				{ "b", desc = ")]} block" },
+				{ "c", desc = "class" },
+				{ "d", desc = "digit(s)" },
+				{ "e", desc = "CamelCase / snake_case" },
+				{ "f", desc = "function" },
+				{ "i", desc = "indent" },
+				{ "o", desc = "block, conditional, loop" },
+				{ "q", desc = "quote `\"'" },
+				{ "t", desc = "tag" },
+				{ "u", desc = "use/call" },
+				{ "{", desc = "{} block" },
+				{ "}", desc = "{} with ws" },
+			}
+
+			---@type wk.Spec[]
+			local ret = { mode = { "o", "x" } }
+			---@type table<string, string>
+			local mappings = vim.tbl_extend("force", {}, {
+				around = "a",
+				inside = "i",
+				around_next = "an",
+				inside_next = "in",
+				around_last = "al",
+				inside_last = "il",
+			}, opts.mappings or {})
+			mappings.goto_left = nil
+			mappings.goto_right = nil
+
+			for name, prefix in pairs(mappings) do
+				name = name:gsub("^around_", ""):gsub("^inside_", "")
+				ret[#ret + 1] = { prefix, group = name }
+				for _, obj in ipairs(objects) do
+					local desc = obj.desc
+					if prefix:sub(1, 1) == "i" then
+						desc = desc:gsub(" with ws", "")
+					end
+					ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
+				end
+			end
+			wk.add(ret, { notify = false })
+		end,
+	},
 }
