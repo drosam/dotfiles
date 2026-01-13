17629ee46af34f946f8293997cd22ff566fc0585
David Rosa Moncayo <david.rosamoncayo@gmail.com>
2026-01-13 22:40:43 +0000

    Show OS in tmux

---
+++ tmux/dot-config/tmux/session_icons.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Detect host OS
+case "$(uname -s)" in
+  Darwin) OS_ICON="󰀵" ;;
+  Linux)
+    if grep -qi ubuntu /etc/os-release 2>/dev/null; then
+      OS_ICON=""
+    else
+      OS_ICON=""
+    fi
+    ;;
+  *) OS_ICON="" ;;
+esac
+
+# Detect Docker
+if [ -f /.dockerenv ] || grep -qE "(docker|kubepods)" /proc/1/cgroup 2>/dev/null; then
+  DOCKER_ICON="  "
+else
+  DOCKER_ICON=""
+fi
+
+# Print stacked icons (Catppuccin will color them)
+printf "#[fg=#{@thm_green}]#[fg=#{@thm_crust},bg=#{@thm_green}]%s%s" "$OS_ICON" "$DOCKER_ICON"
+
