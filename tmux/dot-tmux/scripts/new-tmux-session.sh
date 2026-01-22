5e4af97e792f4dbb838b681499ea4fd232938912
David Rosa Moncayo <david.rosamoncayo@gmail.com>
2026-01-22 23:04:51 +0000

    User tmux sessionizer

---
+++ tmux/dot-tmux/scripts/new-tmux-session.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PROJECTS_DIR="$HOME/Developer"
+
+# fuzzy find a project in PROJECTS_DIR and create a new tmux session in that dir with the project name
+# 
+PROJECTS=$(ls -l "$PROJECTS_DIR" | grep '^d' | awk '{print " ", $NF}')
+
+TMUXINATORS=$(for f in "$HOME/.config/tmuxinator"/*; do
+  [ -f "$f" ] && echo " ${f##*/}" | sed 's/\.[^.]*$//'
+done)
+
+SELECTED=$(printf "$PROJECTS\n$TMUXINATORS" | fzf --tmux)
+
+if [[ $SELECTED == *""* ]]; then
+  tmuxinator $(echo $SELECTED | awk '{print $2}')
+else
+  SELECTED=$(echo $SELECTED | awk '{print $2}')
+
+  if [ $TMUX ]; then
+    tmux new-session -d -s $SELECTED -c "$PROJECTS_DIR/$SELECTED"
+    tmux switch-client -t $SELECTED
+  else
+    tmux new-session -As $SELECTED -c "$PROJECTS_DIR/$SELECTED"
+  fi
+fi
---
+++ tmux/dot-tmux/scripts/tmux-switch-session.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+tmuxsessions=$(tmux list-sessions -F "#{session_name}")
+
+tmux_switch_to_session() {
+    session="$1"
+    if [[ $tmuxsessions = *"$session"* ]]; then
+        tmux switch-client -t "$session"
+    fi
+}
+
+CURRENT_SESSION=$(tmux display-message -p '#S')
+PREV_SESSION=$(cat /tmp/.prev-session)
+
+choice=$(sort -rfu <<< "$tmuxsessions" \
+    | grep -v "^$CURRENT_SESSION$" \
+    | fzf --tmux \
+    | tr -d '\n')
+
+if [ -z "$choice" ]; then
+    exit 0
+fi
+
+echo $CURRENT_SESSION > /tmp/.prev-session
+
+tmux_switch_to_session "$choice"
+
+
