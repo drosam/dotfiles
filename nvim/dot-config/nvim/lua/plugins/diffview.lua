e1626cfb4934dffe42b0843609dc2bc8cd52e98a
David Rosa Moncayo <david.rosamoncayo@gmail.com>
2026-03-04 11:11:01 +0000

    Add more keymaps to diffview

--- nvim/dot-config/nvim/lua/plugins/diffview.lua
+++ nvim/dot-config/nvim/lua/plugins/diffview.lua
@@ -7,6 +7,7 @@ return {
       vim.keymap.set("n", "<leader>ghah", "::DiffviewFileHistory<CR>", { desc = "[GIT] Diff view all file" })
       vim.keymap.set("n", "<leader>dc", ":DiffviewClose<CR>", { desc = "[GIT] Diff view close" })
       vim.keymap.set("n", "<leader>do", ":DiffviewOpen<CR>", { desc = "[GIT] Diff view open" })
+      vim.keymap.set("n", "<leader>db", ":DiffviewOpen origin/main...HEAD<CR>", { desc = "[GIT] Diff branch view open" })
     end,
   },
 }
